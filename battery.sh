00398cc41d563b1f0d72e830d3e04a5628703f2c
oditynet <oditynet@gmail.com>
2024-09-27 07:39:13 +0300

    Update battery.sh
    speak not work

--- battery.sh
+++ battery.sh
@@ -13,9 +13,6 @@ then
                     break
             fi
             i=$((i+1))
-            XDG_RUNTIME_DIR=/run/user/$(id -u)  speaker-test -t sine -f 2000 -l 1
-            sleep 0.5
-            XDG_RUNTIME_DIR=/run/user/$(id -u)  speaker-test -t sine -f 1500 -l 1
             XDG_RUNTIME_DIR=/run/user/$(id -u)  notify-send "[!] BATTERY IS LOW!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
          done
 #else
